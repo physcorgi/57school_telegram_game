16fbc61965940fdcd7ac9ddc3d971019fddb56c5
Dan <76772649+physcorgi@users.noreply.github.com>
2025-02-11 18:01:05 +0300

    Initial commit

---
+++ src/main/kotlin/__kotlin_team_project/afisha/AfishaApplication.kt
@@ -0,0 +1,11 @@
+package __kotlin_team_project.afisha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AfishaApplication
+
+fun main(args: Array<String>) {
+	runApplication<AfishaApplication>(*args)
+}
---
+++ src/test/kotlin/__kotlin_team_project/afisha/AfishaApplicationTests.kt
@@ -0,0 +1,13 @@
+package __kotlin_team_project.afisha
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AfishaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
