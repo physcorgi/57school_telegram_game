30047f3ab3652fe63ce6f7fd2996da5aede9f718
Dan <76772649+physcorgi@users.noreply.github.com>
2025-03-21 23:17:25 +0300

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,6 @@ val jvmTargetVersion = JavaVersion.VERSION_17
 plugins {
     application
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.telegram.bot)
     kotlin("plugin.spring") version "1.9.22"
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
@@ -17,7 +15,7 @@ plugins {
 group = "com.example.blank"
 version = "0.0.1"
 application {
-    mainClass.set("com.example.blank.TgBotApplicationKt")
+    mainClass.set("com.example.blank.ApplicationKt")
 }
 
 repositories {
@@ -44,7 +42,6 @@ dependencies {
     testImplementation("org.mockito:mockito-core:5.2.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
     testImplementation("ch.qos.logback:logback-classic")
-    testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.test {
